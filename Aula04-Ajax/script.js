dafd33d0b94db5d6271ffa5d798d2c86af626d0a
Gabriel Fernando Margoni Lengler <gabriel.lengler@hotmail.com>
2025-08-26 11:11:25 -0300

    Melhorando o código...

--- Aula04-Ajax/script.js
+++ Aula04-Ajax/script.js
@@ -1,6 +1,6 @@
 function lerDados(){
     const req = new XMLHttpRequest()
-    
+
     req.onreadystatechange = function(){
         console.log("readyState atual: " + this.readyState)
         if( this.readyState == 4 && this.status == 200){
@@ -14,6 +14,30 @@ function lerDados(){
 
 
     req.open("GET" , "dados.txt" , true)
+    req.send()
+
+}
+
+function gerarNumeros(){
+    const req = new XMLHttpRequest()
+    
+    req.onreadystatechange = function(){
+        console.log("readyState atual: " + this.readyState)
+        const divNumeros = docmunt.getElementById("numeros")
+
+        if(this.readyState < 4 )
+        divNumeros.innerHTML = "Carregando..."
+        
+        if( this.readyState == 4 && this.status == 200){
+            document.getElementById("conteudo").innerHTML = this.responseText
+        }
+        if( this.readyState == 4 && this.status == 404){
+            txt = this.status + " - " + this.statusText
+            document.getElementById("conteudo").innerHTML = txt
+    }
+}
+    numero = document.getElementById("txtNumero").value
+    req.open("GET" , "servidor.php?valor=" + numero , true)
     req.send()
 
 }
